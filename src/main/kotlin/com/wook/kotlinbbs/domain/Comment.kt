fa920dcd2397fea9d877ed4de778cc8bd00b1f13
Woogie Kim <mdir2kor@gmail.com>
2021-12-02 00:33:03 +0900

    feat(comment): content 프러퍼티 추가

--- src/main/kotlin/com/wook/kotlinbbs/domain/Comment.kt
+++ src/main/kotlin/com/wook/kotlinbbs/domain/Comment.kt
@@ -6,6 +6,7 @@ import javax.persistence.*
 @Table(name = "board_comment")
 class Comment(
     val author: String,
+    var content: String,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(foreignKey = ForeignKey(name = "fk_comment_to_board"))
