6726535ca10617c9a0feee67c926818629173537
Woogie Kim <mdir2kor@gmail.com>
2021-12-02 01:15:00 +0900

    feat(board): 작성자, 제목, 내용이 없으면 예외가 발생한다

--- src/main/kotlin/com/wook/kotlinbbs/domain/Board.kt
+++ src/main/kotlin/com/wook/kotlinbbs/domain/Board.kt
@@ -8,4 +8,14 @@ class Board(
     val author: String,
     var title: String,
     var content: String
-) : BaseEntity(id)
+) : BaseEntity(id) {
+    init {
+        valid()
+    }
+
+    private fun valid() {
+        require(author.isNotBlank()) { "게시물 작성자는 필수값입니다." }
+        require(title.isNotBlank()) { "게시물 제목은 필수값입니다." }
+        require(content.isNotBlank()) { "게시물 내용은 필수값입니다." }
+    }
+}
--- src/test/kotlin/com/wook/kotlinbbs/domain/BoardTest.kt
+++ src/test/kotlin/com/wook/kotlinbbs/domain/BoardTest.kt
@@ -1,12 +1,14 @@
 package com.wook.kotlinbbs.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 class BoardTest {
 
     @Test
-    fun `작성자 제목 내용으로 게시글을 등록한다`() {
+    fun `작성자, 제목, 내용으로 게시글을 등록한다`() {
         //given
         val id = 1L
         val author = "김태욱"
@@ -18,5 +20,26 @@ class BoardTest {
 
         //then
         assertThat(board).isEqualTo(Board(id = id, author = author, title = title, content = content))
+    }
+
+    @Test
+    fun `작성자, 제목, 내용이 없으면 예외가 발생한다`() {
+        assertAll(
+            {
+                assertThatIllegalArgumentException()
+                    .isThrownBy { Board(id = 1L, author = " ", title = "제목 테스트", content = "내용 테스트") }
+                    .withMessageContaining("작성자")
+            },
+            {
+                assertThatIllegalArgumentException()
+                    .isThrownBy { Board(id = 1L, author = "김태욱", title = " ", content = "내용 테스트") }
+                    .withMessageContaining("제목")
+            },
+            {
+                assertThatIllegalArgumentException()
+                    .isThrownBy { Board(id = 1L, author = "김태욱", title = "제목 테스트", content = " ") }
+                    .withMessageContaining("내용")
+            }
+        )
     }
 }
